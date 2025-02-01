2bb8ba772ff975f37561b6c9574f1526f7211a30
yel-m <coin6442@gmail.com>
2025-02-01 16:10:33 +0900

    :fire: 이전 테스트 코드 삭제

--- src/test/java/aurora/carevisionapiserver/util/CameraUtils.java
+++
@@ -1,9 +0,0 @@
-package aurora.carevisionapiserver.util;
-
-import aurora.carevisionapiserver.domain.camera.dto.request.CameraRequest.CameraSelectRequest;
-
-public class CameraUtils {
-    public static CameraSelectRequest createCameraSelectRequest() {
-        return CameraSelectRequest.builder().id("1").build();
-    }
-}
--- src/test/java/aurora/carevisionapiserver/util/HospitalUtils.java
+++
@@ -1,9 +0,0 @@
-package aurora.carevisionapiserver.util;
-
-import aurora.carevisionapiserver.domain.hospital.domain.Hospital;
-
-public class HospitalUtils {
-    public static Hospital createHospital() {
-        return Hospital.builder().id(1L).name("오로라 병원").department("성형외과").build();
-    }
-}
