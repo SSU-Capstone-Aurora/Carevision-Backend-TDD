675d4fb78df720c967579976a73c698625a2fdf4
88dldl <chltnrgml0999@gmail.com>
2025-02-15 17:20:59 +0900

    :white_check_mark: 통합테스트 환경에 NurseService @MockBean 추가

--- src/test/java/aurora/carevisionapiserver/IntegrationTestSupport.java
+++ src/test/java/aurora/carevisionapiserver/IntegrationTestSupport.java
@@ -1,8 +1,13 @@
 package aurora.carevisionapiserver;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+
+import aurora.carevisionapiserver.domain.nurse.service.NurseService;
 
 @ActiveProfiles("local")
 @SpringBootTest
-public abstract class IntegrationTestSupport {}
+public abstract class IntegrationTestSupport {
+    @MockBean protected NurseService nurseService;
+}
