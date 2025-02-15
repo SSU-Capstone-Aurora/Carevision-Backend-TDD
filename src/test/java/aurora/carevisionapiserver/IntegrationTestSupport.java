65e12dd7c12d315b8f648dd44472df9d93cabcfa
88dldl <chltnrgml0999@gmail.com>
2025-02-15 15:53:10 +0900

    [test] 통합 테스트 환경 구성

---
+++ src/test/java/aurora/carevisionapiserver/IntegrationTestSupport.java
@@ -0,0 +1,8 @@
+package aurora.carevisionapiserver;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("local")
+@SpringBootTest
+public abstract class IntegrationTestSupport {}
